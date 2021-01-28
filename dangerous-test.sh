1d69f579218387523546c134d88320a2f9de8a57
Joshua Fox <4542591+JoshuaFox@users.noreply.github.com>
2021-01-28 14:56:36 +0200

    just a pointer

--- dangerous-test.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Generate the script and pipe it directly to bash for execution.
-# This is dangerous, as it does not give you  chance to review the deletion commands
-# before executing them.
-./generate-deletion-script.sh \
-  -k project-viewer-credentials.json \
-  -p joshua-playground | \
-  bash
